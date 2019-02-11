6de7670868cf585e4e1f2c00dad5ef84eaf0d4f1
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-02-11 12:50:11 -0800

    pom modify to correct dependencies

--- src/main/java/com/rest/client/unapec/Application.java
+++ src/main/java/com/rest/client/unapec/Application.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.rest.client.unapec;
 
 /**
@@ -10,5 +5,7 @@ package com.rest.client.unapec;
  * @author DELL
  */
 public class Application {
-    
+    public static void main(String[] args) {
+        
+    }
 }
