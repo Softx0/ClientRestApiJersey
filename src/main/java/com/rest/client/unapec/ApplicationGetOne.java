691c7f1a3891d6e0c402a6d4acea2429a5ab0d01
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-02-11 14:14:16 -0800

    create applicationgetone

--- src/main/java/com/rest/client/unapec/ApplicationGetOne.java
+++ src/main/java/com/rest/client/unapec/ApplicationGetOne.java
@@ -1,5 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.rest.client.unapec;
 
+import java.util.Scanner;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
@@ -8,17 +14,20 @@ import com.sun.jersey.api.client.WebResource;
  *
  * @author DELL
  */
-public class ApplicationGet {
+public class ApplicationGetOne {
 
     private static final String URL_BASE = "http://localhost:8010/api/fundapec/estudiantes";
 
     public static void main(String[] args) {
         try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Ingrese el estudiante: ");
+            String idEstudiante = sc.next();
 
             Client client = Client.create();
 
             WebResource webResource = client
-                    .resource(URL_BASE);
+                    .resource(URL_BASE + "/" + idEstudiante);
 
             ClientResponse response = webResource.accept("application/json")
                     .get(ClientResponse.class);
@@ -30,7 +39,7 @@ public class ApplicationGet {
 
             String output = response.getEntity(String.class);
 
-            System.out.println("Estudiantes Recuperados... \n");
+            System.out.println("Estudiante Recuperado... \n");
             System.out.println(output);
 
         } catch (RuntimeException e) {
